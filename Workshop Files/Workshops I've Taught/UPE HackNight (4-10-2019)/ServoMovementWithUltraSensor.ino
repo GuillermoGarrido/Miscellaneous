60b3fcde36c3e469619fb16f1b4d7314ef72680c
Guillermo Garrido <GuillermoGarrido@users.noreply.github.com>
2019-04-10 10:43:33 -0400

    Create ServoMovementWithUltraSensor.ino
    By using an Arduino Uno, you can make a servo motor move in different directions using your hand and an ultrasonic distance sensor.

---
+++ Workshop Files/Workshops I've Taught/UPE HackNight (4-10-2019)/ServoMovementWithUltraSensor.ino
@@ -0,0 +1,46 @@
+/*By using an Arduino Uno, you can make a servo motor move in different
+directions using your hand and an ultrasonic distance sensor. */
+
+#include <Servo.h>
+
+Servo myServo;
+int trigPin = 2;
+int echoPin = 4;
+long distance;
+long duration;
+ 
+void setup() 
+{
+  myServo.attach(9); 
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);// put your setup code here, to run once:
+}
+ 
+void loop() 
+{
+  ultrasonic();
+  myServo.write(0);
+  if(distance <= 5)
+  {
+    myServo.write(45);
+  }
+  else if(distance <= 10 && distance > 5) 
+  {
+    myServo.write(90);
+  }
+  
+}
+ 
+void ultrasonic()
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration*0.0343/2;
+  Serial.print(distance);
+  Serial.print(" cm");
+  Serial.println("");
+}
