2964c55284b8d0ec8ff61756554311843226184f
Guillermo Garrido <GuillermoGarrido@users.noreply.github.com>
2019-04-11 14:22:48 -0400

    Update ServoMovementWithUltraSensor.ino

--- Workshop Files/Workshops I've Taught/UPE HackNight (4-10-2019)/ServoMovementWithUltraSensor.ino
+++ Workshop Files/Workshops I've Taught/UPE HackNight (4-10-2019)/ServoMovementWithUltraSensor.ino
@@ -4,43 +4,35 @@ directions using your hand and an ultrasonic distance sensor. */
 #include <Servo.h>
 
 Servo myServo;
-int trigPin = 2;
-int echoPin = 4;
+int trigPin = 4;
+int echoPin = 2;
 long distance;
 long duration;
- 
-void setup() 
-{
-  myServo.attach(9); 
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  myServo.attach(10);
   pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);// put your setup code here, to run once:
+  pinMode(echoPin, INPUT);
 }
- 
-void loop() 
-{
+
+void loop() {
+  // put your main code here, to run repeatedly:
   ultrasonic();
   myServo.write(0);
-  if(distance <= 5)
-  {
-    myServo.write(45);
-  }
-  else if(distance <= 10 && distance > 5) 
-  {
+  if(distance <= 10) {
     myServo.write(90);
   }
-  
-}
- 
-void ultrasonic()
-{
+  }
+
+void ultrasonic() {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
+
   duration = pulseIn(echoPin, HIGH);
   distance = duration*0.0343/2;
-  Serial.print(distance);
-  Serial.print(" cm");
-  Serial.println("");
 }
